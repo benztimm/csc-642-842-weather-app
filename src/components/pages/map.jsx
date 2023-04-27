b3bf19f51ae150925a45e8117c89b858050f2cf7
Ekarat Buddharuksa <ebuddharuksa@sfsu.edu>
2023-04-27 03:05:05 -0700

    add date to map, allow forecast or historic map

--- src/components/pages/map.jsx
+++ src/components/pages/map.jsx
@@ -1,47 +1,81 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { API_KEY } from '../statics/data.js';
 import Legend from '../Legends/legend.jsx';
-
+import Button from '@mui/material/Button';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 const WeatherMap = () => {
-    const [selectedLayer, setSelectedLayer] = useState('TA2');
+  const [selectedLayer, setSelectedLayer] = useState('TA2');
+  const [date, setDate] = useState(Math.floor(Date.now() / 1000));
+  const [url, setUrl] = useState(`http://maps.openweathermap.org/maps/2.0/weather/TA2/{z}/{x}/{y}?appid=${API_KEY}&date=${date}`);
 
 
-    useEffect(() => {
-      const interval = setInterval(() => {
-          const storedUnit = sessionStorage.getItem('setting');
-          if (storedUnit===null){
-            sessionStorage.setItem("setting", "TA2");
-        }
-          if (selectedLayer !== storedUnit) {
-            setSelectedLayer(storedUnit);
-          }
-      }, 100); // Check for updates every 100ms (0.1 second)
-      // Clean up the interval when the component unmounts
-      return () => clearInterval(interval);
-  }, [selectedLayer]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const storedUnit = sessionStorage.getItem('setting');
+      if (storedUnit === null) {
+        sessionStorage.setItem("setting", "TA2");
+      }
+      if (selectedLayer !== storedUnit) {
+        setSelectedLayer(storedUnit);
+      }
+    }, 100); // Check for updates every 100ms (0.1 second)
+    // Clean up the interval when the component unmounts
+    setUrl(`http://maps.openweathermap.org/maps/2.0/weather/${selectedLayer}/{z}/{x}/{y}?appid=${API_KEY}&date=${date}`)
+    return () => clearInterval(interval);
+  }, [selectedLayer, date]);
 
-    const weatherLayerUrl =`http://maps.openweathermap.org/maps/2.0/weather/${selectedLayer}/{z}/{x}/{y}?appid=${API_KEY}`
+  const weatherLayerUrl = `http://maps.openweathermap.org/maps/2.0/weather/${selectedLayer}/{z}/{x}/{y}?appid=${API_KEY}`
 
-    return (
+  return (
+    <div style={{ marginTop: ' 75px' }}>
+      <Button variant="contained"
+        onClick={() => { setDate(date - 86400); }}
+        sx={{ marginRight: "10px" }}>
+        <ArrowBackIcon />
+      </Button>
+
+      {(new Date(date * 1000)).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      })}
+
+      <Button
+        variant="contained"
+        onClick={() => { setDate(date + 86400); }}
+        sx={{ marginLeft: "10px" }}>
+        <ArrowForwardIcon />
+      </Button>
+      <Button
+        variant="outlined"
+        onClick={() => { setDate(Math.floor(Date.now() / 1000)) }}
+        sx={{ marginLeft: "10px" }}>
+        Reset
+      </Button>
+      <div style={{ marginBottom: '5px' }}></div>
+
+
       <MapContainer
         center={[37.7749, -122.4194]} // Set your desired map center (latitude, longitude)
         zoom={13}
-        style={{ height: '92vh', width: '100%', marginTop: '70px' }}
+        style={{ height: '85vh', width: '100%', }}
       >
+
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
         <TileLayer
-          url={weatherLayerUrl}
-          opacity={selectedLayer === 'HRD0'?0.6:0.8}
+          url={url}
+          opacity={selectedLayer === 'HRD0' ? 0.6 : 0.8}
         />
-          <Legend />
+        <Legend />
       </MapContainer>
-      
-    );
-  }
-  
-  export default WeatherMap;
+    </div>
+  );
+}
+
+export default WeatherMap;
